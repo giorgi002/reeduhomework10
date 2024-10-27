29f1d9ae2cb1cc4d8e3f0b50f355e454c1d863b9
giorgi002 <167451976+giorgi002@users.noreply.github.com>
2024-10-27 12:10:17 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+// second task 👇
+// function  checkAnswer(text,a,b,useranswer){
+//     let correctanswer;
+
+//     if(text === "what is a plus b?"){
+//         correctanswer = a+b;
+//     } else if(text === "what is a minus b?"){
+//         correctanswer = a-b;
+//     } else{
+//         console.log("incorrect question");
+//     }
+
+//     if(useranswer === correctanswer){
+//         console.log("you are human");
+//     }else{
+//         console.log("you are robot");
+//     }
+// }
+// checkAnswer("what is a plus b?",5,3,8);
+// checkAnswer("what is a minus b?",5,3,2);
+// checkAnswer("what is a plus b?",5,3,10);
+
+
+
+// third task 👇
+// function drawrectangle(height,width){
+//     for(let i =0; i < height; i++){
+//         let row = '';
+    
+//     for(let k =0; k<width; k++){
+//         row +='#'
+//     }
+//     console.log(row);
+// }
+// }
+// drawrectangle(2,2);
+// drawrectangle(3,4);
+
+
+
+// fourth task 👇
+// function isWide(number) {
+//     let numDigits = 0;
+//     let sumDigits = 0;
+//     let n = Math.abs(number); 
+
+//     while (n > 0) {
+//         sumDigits += n % 10; 
+//         n = Math.floor(n / 10); 
+//         numDigits++; 
+//     }
+//     return numDigits > sumDigits;
+// }
+
+// console.log(isWide(123)); 
+// console.log(isWide(101)); 
+// console.log(isWide(301)); 
